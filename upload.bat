2bb62fb2461712695f966ac9d9656f74310f815a
Charlie Taylor <sonofeft@users.noreply.github.com>
2025-02-26 15:28:35 -0800

    fixed intersphinx_mapping in conf.py

--- upload.bat
+++ upload.bat
@@ -1,6 +1,10 @@
-SET "MYPYTHONPATH=D:\Python37_64" 
+setlocal
+
+SET "MYPYTHONPATH=C:\Python310" 
 set PATH=%MYPYTHONPATH%;%MYPYTHONPATH%\Scripts;C:\Windows\System32
 
 python setup.py sdist bdist_wheel
-python -m twine upload --skip-existing dist/*
+:: python -m twine upload --skip-existing dist/*
+C:\Python310\Scripts\twine.exe upload --config-file "%HOMEPATh%\.pypirc" --skip-existing dist/*.*
 
+endlocal
